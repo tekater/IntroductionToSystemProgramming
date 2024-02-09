664d25d744d2ae067bc948a6d666b34ece01f374
tekater <tekateplay1@yandex.ru>
2024-02-09 23:21:26 +0900

    Add project files.

---
+++ Process/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Process
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.rtbProcessName = new System.Windows.Forms.RichTextBox();
+			this.btnStart = new System.Windows.Forms.Button();
+			this.btnStop = new System.Windows.Forms.Button();
+			this.lblProcessInfo = new System.Windows.Forms.Label();
+			this.myProcess = new System.Diagnostics.Process();
+			this.SuspendLayout();
+			// 
+			// rtbProcessName
+			// 
+			this.rtbProcessName.Location = new System.Drawing.Point(12, 12);
+			this.rtbProcessName.Name = "rtbProcessName";
+			this.rtbProcessName.Size = new System.Drawing.Size(100, 24);
+			this.rtbProcessName.TabIndex = 0;
+			this.rtbProcessName.Text = "calc.exe";
+			// 
+			// btnStart
+			// 
+			this.btnStart.Location = new System.Drawing.Point(12, 64);
+			this.btnStart.Name = "btnStart";
+			this.btnStart.Size = new System.Drawing.Size(100, 44);
+			this.btnStart.TabIndex = 1;
+			this.btnStart.Text = "Start";
+			this.btnStart.UseVisualStyleBackColor = true;
+			this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+			// 
+			// btnStop
+			// 
+			this.btnStop.Location = new System.Drawing.Point(12, 114);
+			this.btnStop.Name = "btnStop";
+			this.btnStop.Size = new System.Drawing.Size(100, 44);
+			this.btnStop.TabIndex = 2;
+			this.btnStop.Text = "Stop";
+			this.btnStop.UseVisualStyleBackColor = true;
+			this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+			// 
+			// lblProcessInfo
+			// 
+			this.lblProcessInfo.AutoSize = true;
+			this.lblProcessInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.lblProcessInfo.Location = new System.Drawing.Point(135, 12);
+			this.lblProcessInfo.Name = "lblProcessInfo";
+			this.lblProcessInfo.Size = new System.Drawing.Size(94, 20);
+			this.lblProcessInfo.TabIndex = 3;
+			this.lblProcessInfo.Text = "ProcessInfo";
+			// 
+			// myProcess
+			// 
+			this.myProcess.StartInfo.Domain = "";
+			this.myProcess.StartInfo.LoadUserProfile = false;
+			this.myProcess.StartInfo.Password = null;
+			this.myProcess.StartInfo.StandardErrorEncoding = null;
+			this.myProcess.StartInfo.StandardOutputEncoding = null;
+			this.myProcess.StartInfo.UserName = "";
+			this.myProcess.SynchronizingObject = this;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(420, 265);
+			this.Controls.Add(this.lblProcessInfo);
+			this.Controls.Add(this.btnStop);
+			this.Controls.Add(this.btnStart);
+			this.Controls.Add(this.rtbProcessName);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.RichTextBox rtbProcessName;
+		private System.Windows.Forms.Button btnStart;
+		private System.Windows.Forms.Button btnStop;
+		private System.Windows.Forms.Label lblProcessInfo;
+		private System.Diagnostics.Process myProcess;
+	}
+}
+
---
+++ Process/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Process
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+			rtbProcessName.Text = "notepad";
+			AllignText();
+		}
+
+		void InitProcess()
+		{
+			myProcess.StartInfo = new System.Diagnostics.ProcessStartInfo(rtbProcessName.Text);
+		}
+		void AllignText()
+		{
+			rtbProcessName.SelectAll();
+			rtbProcessName.SelectionAlignment = HorizontalAlignment.Center;
+		}
+		private void btnStart_Click(object sender, EventArgs e)
+		{
+			InitProcess();
+			myProcess.Start();
+			Info();
+		}
+
+		private void btnStop_Click(object sender, EventArgs e)
+		{
+			myProcess.CloseMainWindow();	// Закрывает процесс
+			myProcess.Close();				// Освобождает ресурсы, занимаемые процессом
+		}
+
+		private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			btnStop_Click(sender, e);
+		}
+		void Info()
+		{
+			lblProcessInfo.Text = "Process info:\n";
+			lblProcessInfo.Text += $"Name:			{myProcess.ProcessName}			\n";
+			lblProcessInfo.Text += $"PID:					{myProcess.Id}					\n";
+			lblProcessInfo.Text += $"Session Id:			{myProcess.SessionId}			\n";
+			lblProcessInfo.Text += $"Base Priority:			{myProcess.BasePriority}		\n";
+			lblProcessInfo.Text += $"Priority Class:		{myProcess.PriorityClass}		\n";
+			lblProcessInfo.Text += $"Start Time:			{myProcess.StartTime}			\n";
+			lblProcessInfo.Text += $"Total Processor Time:	{myProcess.TotalProcessorTime}	\n";
+			lblProcessInfo.Text += $"User Processor Time:	{myProcess.UserProcessorTime}	\n";
+			lblProcessInfo.Text += $"Affinity:				{myProcess.ProcessorAffinity}	\n";
+			lblProcessInfo.Text += $"Threads:				{myProcess.Threads.Count}		\n";
+		}
+	}
+}
