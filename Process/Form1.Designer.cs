03b2edf86afdbdebb105eee64f33751dbe7f3183
tekater <tekateplay1@yandex.ru>
2024-02-12 22:39:43 +0900

    1. ADD/DEL DONE

--- Process/Form1.Designer.cs
+++ Process/Form1.Designer.cs
@@ -29,51 +29,71 @@
 		private void InitializeComponent()
 		{
 			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
 			this.rtbProcessName = new System.Windows.Forms.RichTextBox();
 			this.btnStart = new System.Windows.Forms.Button();
 			this.btnStop = new System.Windows.Forms.Button();
 			this.lblProcessInfo = new System.Windows.Forms.Label();
 			this.myProcess = new System.Diagnostics.Process();
 			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.splitter1 = new System.Windows.Forms.Splitter();
+			this.splitter2 = new System.Windows.Forms.Splitter();
+			this.panelMainInfo = new System.Windows.Forms.Panel();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.lblPanelInfo = new System.Windows.Forms.Label();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.panel3 = new System.Windows.Forms.Panel();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.panelMainInfo.SuspendLayout();
+			this.panel2.SuspendLayout();
+			this.panel1.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// rtbProcessName
 			// 
-			this.rtbProcessName.Location = new System.Drawing.Point(12, 12);
+			this.rtbProcessName.BackColor = System.Drawing.SystemColors.InactiveCaption;
+			this.rtbProcessName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.rtbProcessName.Location = new System.Drawing.Point(12, 10);
 			this.rtbProcessName.Name = "rtbProcessName";
-			this.rtbProcessName.Size = new System.Drawing.Size(100, 24);
+			this.rtbProcessName.Size = new System.Drawing.Size(128, 31);
 			this.rtbProcessName.TabIndex = 0;
 			this.rtbProcessName.Text = "calc.exe";
 			// 
 			// btnStart
 			// 
-			this.btnStart.Location = new System.Drawing.Point(12, 64);
+			this.btnStart.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+			this.btnStart.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+			this.btnStart.Location = new System.Drawing.Point(151, 10);
 			this.btnStart.Name = "btnStart";
-			this.btnStart.Size = new System.Drawing.Size(100, 44);
+			this.btnStart.Size = new System.Drawing.Size(121, 31);
 			this.btnStart.TabIndex = 1;
 			this.btnStart.Text = "Start";
-			this.btnStart.UseVisualStyleBackColor = true;
+			this.btnStart.UseVisualStyleBackColor = false;
 			this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
 			// 
 			// btnStop
 			// 
-			this.btnStop.Location = new System.Drawing.Point(12, 114);
+			this.btnStop.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+			this.btnStop.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+			this.btnStop.Location = new System.Drawing.Point(278, 10);
 			this.btnStop.Name = "btnStop";
-			this.btnStop.Size = new System.Drawing.Size(100, 44);
+			this.btnStop.Size = new System.Drawing.Size(121, 31);
 			this.btnStop.TabIndex = 2;
 			this.btnStop.Text = "Stop";
-			this.btnStop.UseVisualStyleBackColor = true;
+			this.btnStop.UseVisualStyleBackColor = false;
 			this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
 			// 
 			// lblProcessInfo
 			// 
 			this.lblProcessInfo.AutoSize = true;
 			this.lblProcessInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.lblProcessInfo.Location = new System.Drawing.Point(135, 12);
+			this.lblProcessInfo.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+			this.lblProcessInfo.Location = new System.Drawing.Point(3, 9);
 			this.lblProcessInfo.Name = "lblProcessInfo";
-			this.lblProcessInfo.Size = new System.Drawing.Size(94, 20);
+			this.lblProcessInfo.Size = new System.Drawing.Size(85, 20);
 			this.lblProcessInfo.TabIndex = 3;
-			this.lblProcessInfo.Text = "ProcessInfo";
+			this.lblProcessInfo.Text = "LoadInfo...";
 			// 
 			// myProcess
 			// 
@@ -87,22 +107,105 @@
 			// 
 			// timer1
 			// 
+			this.timer1.Interval = 1000;
 			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Location = new System.Drawing.Point(12, 49);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.Size = new System.Drawing.Size(387, 270);
+			this.dataGridView1.TabIndex = 4;
+			// 
+			// splitter1
+			// 
+			this.splitter1.Location = new System.Drawing.Point(0, 0);
+			this.splitter1.Name = "splitter1";
+			this.splitter1.Size = new System.Drawing.Size(3, 331);
+			this.splitter1.TabIndex = 5;
+			this.splitter1.TabStop = false;
+			// 
+			// splitter2
+			// 
+			this.splitter2.Location = new System.Drawing.Point(3, 0);
+			this.splitter2.Name = "splitter2";
+			this.splitter2.Size = new System.Drawing.Size(3, 331);
+			this.splitter2.TabIndex = 6;
+			this.splitter2.TabStop = false;
+			// 
+			// panelMainInfo
+			// 
+			this.panelMainInfo.BackColor = System.Drawing.SystemColors.Control;
+			this.panelMainInfo.Controls.Add(this.lblProcessInfo);
+			this.panelMainInfo.Location = new System.Drawing.Point(425, 49);
+			this.panelMainInfo.Name = "panelMainInfo";
+			this.panelMainInfo.Size = new System.Drawing.Size(388, 270);
+			this.panelMainInfo.TabIndex = 7;
+			// 
+			// panel2
+			// 
+			this.panel2.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+			this.panel2.Controls.Add(this.lblPanelInfo);
+			this.panel2.Location = new System.Drawing.Point(425, 10);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(388, 31);
+			this.panel2.TabIndex = 8;
+			// 
+			// lblPanelInfo
+			// 
+			this.lblPanelInfo.AutoSize = true;
+			this.lblPanelInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.lblPanelInfo.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+			this.lblPanelInfo.Location = new System.Drawing.Point(3, 4);
+			this.lblPanelInfo.Name = "lblPanelInfo";
+			this.lblPanelInfo.Size = new System.Drawing.Size(98, 20);
+			this.lblPanelInfo.TabIndex = 4;
+			this.lblPanelInfo.Text = "ProcessInfo:";
+			// 
+			// panel1
+			// 
+			this.panel1.Controls.Add(this.panel3);
+			this.panel1.Location = new System.Drawing.Point(405, 0);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(10, 331);
+			this.panel1.TabIndex = 9;
+			// 
+			// panel3
+			// 
+			this.panel3.BackColor = System.Drawing.SystemColors.Control;
+			this.panel3.Location = new System.Drawing.Point(7, -8);
+			this.panel3.Name = "panel3";
+			this.panel3.Size = new System.Drawing.Size(7, 341);
+			this.panel3.TabIndex = 9;
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(420, 265);
-			this.Controls.Add(this.lblProcessInfo);
+			this.BackColor = System.Drawing.SystemColors.GrayText;
+			this.ClientSize = new System.Drawing.Size(821, 331);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.panel2);
+			this.Controls.Add(this.panelMainInfo);
+			this.Controls.Add(this.splitter2);
+			this.Controls.Add(this.splitter1);
+			this.Controls.Add(this.dataGridView1);
 			this.Controls.Add(this.btnStop);
 			this.Controls.Add(this.btnStart);
 			this.Controls.Add(this.rtbProcessName);
+			this.ForeColor = System.Drawing.SystemColors.ControlText;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.Name = "Form1";
 			this.Text = "Form1";
 			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.panelMainInfo.ResumeLayout(false);
+			this.panelMainInfo.PerformLayout();
+			this.panel2.ResumeLayout(false);
+			this.panel2.PerformLayout();
+			this.panel1.ResumeLayout(false);
 			this.ResumeLayout(false);
-			this.PerformLayout();
 
 		}
 
@@ -114,6 +217,14 @@
 		private System.Windows.Forms.Label lblProcessInfo;
 		private System.Diagnostics.Process myProcess;
 		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.Splitter splitter2;
+		private System.Windows.Forms.Splitter splitter1;
+		private System.Windows.Forms.Panel panelMainInfo;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.Label lblPanelInfo;
+		private System.Windows.Forms.Panel panel3;
+		private System.Windows.Forms.Panel panel1;
 	}
 }
 
--- Process/Form1.cs
+++ Process/Form1.cs
@@ -7,21 +7,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace Process
 {
 	public partial class Form1 : Form
 	{
+		List<System.Diagnostics.Process> process_list;
 		public Form1()
 		{
 			InitializeComponent();
 			//rtbProcessName.Text = "notepad";
+			process_list = new List<System.Diagnostics.Process>();
 			AllignText();
+			//InitProcess();
 		}
 
 		void InitProcess()
 		{
+			AllignText();
+			myProcess = new System.Diagnostics.Process();
 			myProcess.StartInfo = new System.Diagnostics.ProcessStartInfo(rtbProcessName.Text);
+			myProcess.Start();
+			process_list.Add(myProcess);
 		}
 		void AllignText()
 		{
@@ -31,34 +39,67 @@ namespace Process
 		private void btnStart_Click(object sender, EventArgs e)
 		{
 			InitProcess();
-			myProcess.Start();
+			//myProcess.Start();
+			timer1.Enabled = true;
+			this.TopMost = true;
 		}
 
 		private void btnStop_Click(object sender, EventArgs e)
 		{
-			myProcess.CloseMainWindow();    // Закрывает процесс
-			//myProcess.WaitForExit();		// Ждёт закрытия процесса
-			myProcess.Close();              // Освобождает ресурсы, занимаемые процессом
-			//myProcess.Kill();				// Секир башка
+			if (process_list.Count > 0)
+			{
+				myProcess = process_list.Last();
+				myProcess.CloseMainWindow();    // Закрывает процесс
+												//myProcess.WaitForExit();		// Ждёт закрытия процесса
+				myProcess.Close();              // Освобождает ресурсы, занимаемые процессом
+				//myProcess.Kill();				// Секир башка
+				process_list.RemoveAt(process_list.Count - 1);
+				
+			}
 		}
 
 		private void Form1_FormClosing(object sender, FormClosingEventArgs e)
 		{
-			btnStop_Click(sender, e);
+			//btnStop_Click(sender, e);
+			/*foreach(System.Diagnostics.Process i in process_list)
+			{
+				i.CloseMainWindow();
+				i.Close();
+				process_list.RemoveAt(0);
+			}*/
+			while(process_list.Count > 0)
+			{
+				process_list.First().CloseMainWindow();
+				process_list.First().Close();
+				process_list.RemoveAt(0);
+			}
 		}
 		void Info() //Выяснить, почему при закрытии калькулятора падает исключение - калькулятор запускается в несколько потоков, поэтому при закрытии первого потока не затрагивает сам процесс.
 		{
-			lblProcessInfo.Text = "Process info:\n";
-			lblProcessInfo.Text += $"Name:					{myProcess.ProcessName}			\n";
-			lblProcessInfo.Text += $"PID:					{myProcess.Id}					\n";
-			lblProcessInfo.Text += $"Session Id:			{myProcess.SessionId}			\n";
-			lblProcessInfo.Text += $"Base Priority:			{myProcess.BasePriority}		\n";
-			lblProcessInfo.Text += $"Priority Class:		{myProcess.PriorityClass}		\n";
-			lblProcessInfo.Text += $"Start Time:			{myProcess.StartTime}			\n";
-			lblProcessInfo.Text += $"Total Processor Time:	{myProcess.TotalProcessorTime}	\n";
-			lblProcessInfo.Text += $"User Processor Time:	{myProcess.UserProcessorTime}	\n";
-			lblProcessInfo.Text += $"Affinity:				{myProcess.ProcessorAffinity}	\n";
-			lblProcessInfo.Text += $"Threads:				{myProcess.Threads.Count}		\n";
+			if (process_list.Count > 0)
+			{
+				myProcess = process_list.First();
+				//lblProcessInfo.Text = "Process info:\n";
+				//lblProcessInfo.Text = "";
+				lblProcessInfo.Text = "Total process Count: "		+ $"{process_list.Count}					\n";
+				lblProcessInfo.Text += "Current Process															\n";
+				lblProcessInfo.Text += "Name: "						+ $"{myProcess.ProcessName}					\n";
+				lblProcessInfo.Text += "PID: "						+ $"{myProcess.Id}							\n";
+				lblProcessInfo.Text += "Session Id: "				+ $"{myProcess.SessionId}					\n";
+				lblProcessInfo.Text += "Base Priority: "			+ $"{myProcess.BasePriority}				\n";
+				lblProcessInfo.Text += "Priority Class: "			+ $"{myProcess.PriorityClass}				\n";
+				lblProcessInfo.Text += "Start Time: "				+	$"{myProcess.StartTime.Hour}:"			+
+																		$"{myProcess.StartTime.Minute}			\n";
+				lblProcessInfo.Text += "Total Processor Time: "		+	$"{myProcess.TotalProcessorTime.Seconds}\n";
+				lblProcessInfo.Text += "User Processor Time: "		+	$"{myProcess.UserProcessorTime.Seconds}	\n";
+				lblProcessInfo.Text += "Affinity: "					+	$"{myProcess.ProcessorAffinity}			\n";
+				lblProcessInfo.Text += "Threads: "					+	$"{myProcess.Threads.Count}				\n";
+			}
+			else
+			{
+				lblProcessInfo.Text = "LoadInfo...";
+				timer1.Enabled = false;
+			}
 		}
 
 		private void timer1_Tick(object sender, EventArgs e)
